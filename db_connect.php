e9128f903e3a7ce8502a213ea421729d617204fa
Yqqq23 <124893185+Yqqq23@users.noreply.github.com>
2024-03-26 16:30:41 +0800

    Update db_connect.php

--- db_connect.php
+++ db_connect.php
@@ -1,7 +1,15 @@
 <?php
+$servername = "localhost";
+$username = "WY";
+$password = "qi3yun2wang";
+$dbname = "foodshare";
 
-/* 
- * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
- * Click nbfs://nbhost/SystemFileSystem/Templates/Scripting/EmptyPHP.php to edit this template
- */
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+?>
 
