dbfd5011c464e87a13f6d3fb4c04e2bafbeb2826
Yqqq23 <124893185+Yqqq23@users.noreply.github.com>
2024-04-07 21:36:41 +0800

    Update sign_sto.php

--- sign_sto.php
+++ sign_sto.php
@@ -6,38 +6,75 @@ function get_post($conn, $var)
     return $conn->real_escape_string($_POST[$var]);
 }
 
-if (isset($_POST['Store_name']) &&
+if (isset($_POST['user_name']) &&
+    isset($_POST['Store_name']) &&
     isset($_POST['Store_kind']) &&
     isset($_POST['Location']) &&
     isset($_POST['Head_name']) &&
-    isset($_POST['Head_Contact_num'])) {
-
+    isset($_POST['Head_Contact_num']) &&
+    isset($_POST['Head_Identity_num']) &&
+    isset($_POST['password'])) {
+    
+    $user_name = get_post($conn, 'user_name');
     $Store_name = get_post($conn, 'Store_name');
     $Store_kind = get_post($conn, 'Store_kind');
     $Location = get_post($conn, 'Location');
     $Head_name = get_post($conn, 'Head_name');
     $Head_Contact_num = get_post($conn, 'Head_Contact_num');
+    $Head_Identity_num = get_post($conn, 'Head_Identity_num');
+    $password = get_post($conn, 'password');
+    $user_type = 'Store';
+    
+    // 检查Store_name是否已存在
+    $query = "SELECT * FROM volunteer WHERE user_name = '$user_name'";
+    $result = mysqli_query($conn, $query);
 
-    $query = "INSERT INTO classics (Store_name,Store_kind, Location, Head_name, Head_Contact_num) VALUES (?, ?, ?, ?, ?)";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("sssss", $Store_name, $Store_kind, $Location, $Head_name, $Head_Contact_num);
-    if ($stmt->execute()) {
-        echo "Record inserted successfully!<br><br>";
+    if ($result && $result->num_rows > 0) {
+        echo "The store name is already taken. Please choose another.";
     } else {
-        echo "INSERT failed: $query<br>" . $stmt->error . "<br><br>";
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+    
+        $query = "INSERT INTO login (User_Name,Password, User_type) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("sss", $user_name, $hashedPassword, $user_type);
+        if ($stmt->execute()) {
+           
+        } else {
+            echo "INSERT failed: $query<br>" . $stmt->error . "<br><br>";
+        }
+        $stmt->close();
     }
-    $stmt->close();
+        
+        $query_1 = "SELECT User_id FROM login WHERE User_Name = '$user_name';";
+        $result = mysqli_query($conn, $query_1);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        $User_id = $row[0];
+        $query = "INSERT INTO store (User_id, User_type, User_name, Store_name,Store_kind, Location, Head_name, Head_Contact_num, Head_Identity_num,  password) VALUES (?, ?, ?, ?, ?, ?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("isssssssss", $User_id, $user_type, $user_name, $Store_name, $Store_kind, $Location, $Head_name, $Head_Contact_num, $Head_Identity_num, $hashedPassword);
+        if ($stmt->execute()) {
+            echo "Record inserted successfully!<br><br>";
+        } else {
+            echo "INSERT failed: $query<br>" . $conn->error . "<br><br>";
+        }
+        $stmt->close();
+        
+
 }
 
 // 下面是表单HTML
 echo <<<_END
-<form action="sign_S.php" method="post">
+<form action="S.php" method="post">
 <pre>
+User_type: Store
+user_name <input type="text" name="user_name">
+password <input type="text" name="password">
 Store_name <input type="text" name="Store_name">
 Store_kind <input type="text" name="Store_kind">
 Location <input type="text" name="Location">
 Head_name <input type="text" name="Head_name">
 Head_Contact_num <input type="text" name="Head_Contact_num">
+Head_Identity_num <input type="text" name="Head_Identity_num">
 <input type="submit" value="ADD RECORD">
 </pre>
 <form action="register.php" method="POST">
@@ -47,35 +84,4 @@ Head_Contact_num <input type="text" name="Head_Contact_num">
 </form>
 _END;
 
-// 下面是获取并显示数据库中记录的逻辑
-$query = "SELECT * FROM classics";
-$result = $conn->query($query);
-
-if (!$result) {
-    die("Database access failed: " . $conn->error);
-}
-
-$rows = $result->num_rows;
-
-for ($j = 0; $j < $rows; ++$j) {
-    $result->data_seek($j);
-    $row = $result->fetch_array(MYSQLI_NUM);
-
-    echo <<<_END
-<pre>
-Store_name: $row[0]
-Store_kind: $row[1]
-Location: $row[2]
-Head_name: $row[3]
-Head_Contact_num: $row[4]
-</pre>
-<form action="sign_S.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="isbn" value="$row[4]">
-</form>
-_END;
-}
-
-$result->close();
-$conn->close();
 ?>
