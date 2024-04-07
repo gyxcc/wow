7f3ec389addf1d5140404403f517aca1749a1127
Yqqq23 <124893185+Yqqq23@users.noreply.github.com>
2024-04-07 21:36:59 +0800

    Update sign_volun.php

--- sign_volun.php
+++ sign_volun.php
@@ -6,72 +6,70 @@ function get_post($conn, $var)
     return $conn->real_escape_string($_POST[$var]);
 }
 
-if (isset($_POST['User_id']) &&
-    isset($_POST['user_name']) &&
+if (isset($_POST['user_name']) &&
     isset($_POST['user_contact']) &&
-    isset($_POST['identity'])) {
-
-    $User_id = get_post($conn, 'User_id');
+    isset($_POST['identity']) &&
+    isset($_POST['password'])) {
+    
     $user_name = get_post($conn, 'user_name');
     $user_contact = get_post($conn, 'user_contact');
     $identity = get_post($conn, 'identity');
+    $password = get_post($conn, 'password');
+    $user_type = 'Volunteer';
 
-    $query = "INSERT INTO classics (User_id, user_name, user_contact, identity) VALUES (?, ?, ?, ?)";
-    $stmt = $conn->prepare($query);
-    $stmt->bind_param("ssss", $User_id, $user_name, $user_contact, $identity);
-    if ($stmt->execute()) {
-        echo "Record inserted successfully!<br><br>";
+    // 检查user_name是否已存在
+    $query = "SELECT * FROM volunteer WHERE user_name = '$user_name'";
+    $result = mysqli_query($conn, $query);
+
+    // 如果$result有一行或多行数据，说明user_name已存在
+    if ($result && mysqli_num_rows($result) > 0) {
+        echo "The username is already taken. Please choose another.";
     } else {
-        echo "INSERT failed: $query<br>" . $stmt->error . "<br><br>";
+        // 如果user_name不存在，则执行插入操作
+        $hashedPassword = password_hash($password, PASSWORD_DEFAULT);
+        
+        $query1 = "INSERT INTO login (User_Name,Password, User_type) VALUES (?, ?, ?)";
+        $stmt = $conn->prepare($query1);
+        $stmt->bind_param("sss", $user_name, $hashedPassword, $user_type);
+        if ($stmt->execute()) {
+        } else {
+            echo "INSERT failed: $query1<br>" . $stmt->error . "<br><br>";
+        }
+        $stmt->close();
+       
+        $query_2 = "SELECT User_id FROM login WHERE User_Name = '$user_name '";
+        $result = mysqli_query($conn, $query_2);
+        $row = $result->fetch_array(MYSQLI_NUM);
+        $User_id = $row[0];
+        $query = "INSERT INTO volunteer (user_name, user_contact_num, identity, password, user_type) VALUES (?, ?, ?, ?, ?)";
+        $stmt = $conn->prepare($query);
+        $stmt->bind_param("sssss", $user_name, $user_contact, $identity, $hashedPassword, $user_type);
+        if ($stmt->execute()) {
+            echo "Record inserted successfully!<br><br>";
+        } else {
+            echo "INSERT failed: $query<br>" . $stmt->error . "<br><br>";
+        }
+        $stmt->close();
+        
+        
     }
-    $stmt->close();
 }
 
+// 这里是HTML表单的代码
 echo <<<_END
-<form action="sign_S.php" method="post">
+<form action="V.php" method="post">
+
 <pre>
-User_id <input type="text" name="User_id">
+user_type: Volunteer 
 user_name <input type="text" name="user_name">
+password <input type="text" name="password">
 user_contact <input type="text" name="user_contact">
 identity <input type="text" name="identity">
 <input type="submit" value="ADD RECORD">
 </pre>
-</form>
 <form action="login.php" method="get">
     <p>Already have an account? Click <input type="submit" value="Login"></p>
 </form>
 _END;
 
-$query = "SELECT * FROM classics";
-$result = $conn->query($query);
-
-if (!$result) {
-    die("Database access failed: " . $conn->error);
-}
-
-$rows = $result->num_rows;
-
-for ($j = 0; $j < $rows; ++$j) {
-    $result->data_seek($j);
-    $row = $result->fetch_array(MYSQLI_NUM);
-
-    echo <<<_END
-<pre>
-User_id: $row[0]
-user_name: $row[1]
-user_contact: $row[2]
-identity: $row[3]
-</pre>
-<form action="sign_S.php" method="post">
-<input type="
-</pre>
-<form action="sign_S.php" method="post">
-<input type="hidden" name="delete" value="yes">
-<input type="hidden" name="isbn" value="$row[4]">
-</form>
-_END;
-}
-
-$result->close();
-$conn->close();
 ?>
